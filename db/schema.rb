03d15759e48b4b716e4e63e0e930c7b49576fdfd
kensukemt2 <kensukemt2@gmail.com>
2018-05-26 05:16:39 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516185932) do
+ActiveRecord::Schema.define(version: 20180525165721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bord_comments", force: :cascade do |t|
     t.integer "user_id"
-    t.text "comment"
+    t.text "content"
     t.integer "trouble_bord_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
