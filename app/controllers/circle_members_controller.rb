513fcd836fdad35ee59bb46a1ad34c0a87304a60
kensukemt2 <kensukemt2@gmail.com>
2018-08-02 17:07:15 +0900

    issue#7について修正

--- app/controllers/circle_members_controller.rb
+++ app/controllers/circle_members_controller.rb
@@ -1,10 +1,10 @@
 class CircleMembersController < ApplicationController
   def create
     circlemember = current_user.circle_members.create(circle_id: params[:circle_id])
-    redirect_to user_path(current_user.id), notice: "サークルに参加しました"
+    redirect_to user_path(current_user), notice: 'サークルに参加しました'
   end
   def destroy
     circlemember = current_user.circle_members.find_by(circle_id: params[:circle_id]).destroy
-    redirect_to user_path(current_user.id), notice: "サークルをやめました"
+    redirect_to user_path(current_user), notice: 'サークルをやめました'
   end
 end
